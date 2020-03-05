90a9835f98bf7cc36de4574de31e9f60fa469fbe
manos <manolis.trungu@socital.com>
2020-03-05 21:25:21 +0200

    Change bryct require to bcryptjs

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const User = require('../../models/user');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
 
